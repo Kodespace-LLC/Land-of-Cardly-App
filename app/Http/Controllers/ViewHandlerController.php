def8bd1ae5bfabdef392dc4d6c2b593bba0a451d
alibukhari1995 <95312876+alibukhari1995@users.noreply.github.com>
2022-03-22 02:58:33 +0500

    csvfile2.0

--- app/Http/Controllers/ViewHandlerController.php
+++ app/Http/Controllers/ViewHandlerController.php
@@ -47,30 +47,42 @@ class ViewHandlerController extends Controller
         
             $file = $request->file('csv');
             // \Log::debug($file);
-            $savefile= $file->getClientOriginalName();
+            $savefile= time()."_".$file->getClientOriginalName();
             $path="uploads";
             // now you have access to the file being uploaded
             //perform the upload operation.
             $file->move($path,$savefile);
            $csvdata= $this->readcsv($path,$savefile);
            \Log::debug($csvdata);
-           if($csvdata){
-               unlink(public_path($path."/".$savefile));
-           }
-           return response()->json($csvdata);
+        //    if($csvdata){
+        //        unlink(public_path($path."/".$savefile));
+        //    }
+           return response()->json([
+               "length"=>count($csvdata),
+               "filename"=>$savefile
+           ]);
             
     }
     public function readcsv($path,$savefile){
-        $emails=[];
+        $recipientdata=[];
         if(($open=fopen(public_path()."/".$path."/".$savefile,"r"))!==FALSE){
             while(($data=fgetcsv($open,1000,","))!==FALSE){
-                $emails[]=$data;
+                $recipientdata[]=[
+                    "firstName"=>$data[0],
+                    "lastName"=>$data[1],
+                    "address"=>$data[2],
+                    "address2"=>$data[3],
+                    "city"=>$data[4],
+                    "region"=>$data[5],
+                    "postcode"=>$data[6],
+                    "country"=>$data[7],
+                ];
                 
 
             }
             fclose($open);
         }
        
-        return($emails);
+        return($recipientdata);
     }
 }
